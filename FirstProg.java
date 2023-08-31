c0456fe0d6ab7e1467ee763a48c9a2cf9aaa9c68
anarrhmaaz <143683697+anarrhmaaz@users.noreply.github.com>
2023-08-31 12:20:43 +0700

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class Firstprog{
+  public static void main( String [] args){
+    System.out.printIn("I am determined to learn how to code.");
+    System.out.printIn("Todays's date is");
+  }
+}
